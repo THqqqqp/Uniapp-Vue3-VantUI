0b57f097207f4f310c79105aba3e70b2849cc531
THqqqqp <2472412112@qq.com>
2024-07-12 21:17:32 +0800

    清除默认页面信息

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
-<script>
-export default {
-  onLaunch: function () {
-    console.log('App Launch')
-  },
-  onShow: function () {
-    console.log('App Show')
-  },
-  onHide: function () {
-    console.log('App Hide')
-  },
-}
+<script setup>
+
 </script>
 
-<style>
+<template>
+
+</template>
+
+<style lang="scss" scoped>
 /*每个页面公共css */
 </style>
