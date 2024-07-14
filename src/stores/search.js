739df9d30c05959cf5f9f53de389e92e925072cb
THqqqqp <2472412112@qq.com>
2024-07-14 22:30:10 +0800

    引入pinia进行全局数据管理

---
+++ src/stores/search.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+
+// 为一个简单的pinia使用示例
+export const useCounterStore = defineStore('counter', {
+    state: () => ({
+        count: 0
+    }),
+    actions: {
+        increment() {
+            this.count++
+        }
+    }
+})
