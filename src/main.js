7552dede851398294a023bbf03d2325c159ec082
THqqqqp <2472412112@qq.com>
2024-07-14 22:30:10 +0800

    引入pinia进行全局数据管理

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import {
-	createSSRApp
-} from "vue";
+import {createSSRApp} from "vue";
 import App from "./App.vue";
 import {router} from "@/router";
+import {createPinia} from "pinia";
+
 export function createApp() {
-	const app = createSSRApp(App);
-	app.use(router)
-	return {
-		app,
-	};
+    const app = createSSRApp(App);
+    const pinia = createPinia()
+
+    app.use(router)
+    app.use(pinia)
+    return {
+        app,
+    };
 }
